4b07fd592f612d5d15b3044c7ffa0d7512a10eb2
Lucas Oliveira <lucas.santos661@hotmail.com>
2024-03-10 21:31:18 -0300

    Game seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_game(game_year, score, genre, img_url, long_description, platforms, short_description, title)VALUES(2012, 4.8, 'Role Playing (RPG), Shooter', 'https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/1.png', 'Jogo de tiro', 'XBox, PlayStation, PC', 'Jogo', 'Mass Effect Trilogy');
